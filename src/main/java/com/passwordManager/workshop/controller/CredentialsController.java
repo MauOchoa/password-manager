2df7dc99c221ebf4d3801e450535d646318092ff
Mau <mauricio.ochoaen@udlap.mx>
2021-08-16 07:57:04 -0500

    fix: imports removed, and project from HP

--- src/main/java/com/passwordManager/workshop/controller/CredentialsController.java
+++ src/main/java/com/passwordManager/workshop/controller/CredentialsController.java
@@ -3,7 +3,6 @@ package com.passwordManager.workshop.controller;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,9 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.passwordManager.workshop.models.Credentials;
 import com.passwordManager.workshop.security.JwtTokenProvider;
 import com.passwordManager.workshop.service.CredentialsService;
-
-
-
 
 @RestController
 public class CredentialsController {
